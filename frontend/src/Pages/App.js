4afaf5408c490ebb2e3aea98f8afd3452d6c2c7a
henrynguyen <anguye22@uw.edu>
2023-04-22 16:37:23 -0700

    Henry-code-table

---
+++ frontend/src/Pages/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Table from './Table.js';
+//import filter from './Filter.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className = "Filter">
+      </header>
+      <header className="Table">
+          <Table/>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
